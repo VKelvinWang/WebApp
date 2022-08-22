f53ec480f524f0aeb1bd6516521f174d1bc9126b
Vinsensius Kelvin Wang <62001746+VKelvinWang@users.noreply.github.com>
2022-08-22 22:33:29 +1000

    initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+//DOM compilation js
+const express=require("express"); 
+const ejs=require("ejs");
+const bodyParser=require("body-parser");
+const app = express();
+const path = require('path');
+const PORT = 3000;
+
+app.get('/', function (req, res){
+        res.sendFile(path.join(__dirname+'/index.html'));
+});
+  
+app.listen(PORT, (error) =>{
+    if(!error)
+        console.log("Server is Successfully Running, and App is listening on port "+ PORT)
+    else 
+        console.log("Error occurred, server can't start", error);
+    }
+);
